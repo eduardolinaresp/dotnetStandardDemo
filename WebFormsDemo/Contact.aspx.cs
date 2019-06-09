f69e0a80490c59c8aa587fda87bad4250506b63d
Eduardo <eduardo.linares@outlook.com>
2019-06-09 18:53:07 -0400

    ADD pdf to WEBForms

--- WebFormsDemo/Contact.aspx.cs
+++ WebFormsDemo/Contact.aspx.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +16,95 @@ namespace WebFormsDemo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                ImprimirPdf();
+
+            }
+        }
+
+        private void ImprimirPdf()
+        {
+            PdfDocument document = new PdfDocument();
+
+            document.Info.Title = "PDFsharp Demo";
+            document.Info.Title = "PDFsharp ";
+
+            document.Info.Author = "PDFsharp";
+
+            document.Info.Subject = "Server time: " + 1;
+
+            //PdfPage page = document.AddPage();
+
+            //page.Width = XUnit.FromMillimeter(200);
+
+            //page.Height = XUnit.FromMillimeter(200);
+
+            SamplePage1(document);
+
+            MemoryStream stream = new MemoryStream();
+
+            document.Save(stream, false);
+
+            Response.Clear();
+
+            Response.ContentType = "application/pdf";
+
+            Response.AddHeader("content-length", stream.Length.ToString());
+
+            Response.BinaryWrite(stream.ToArray());
+
+            Response.Flush();
+
+            stream.Close();
+            
+            Response.End();
+
 
         }
+
+
+        static void SamplePage1(PdfDocument document)
+        {
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            // HACK²
+            gfx.MUH = PdfFontEncoding.Unicode;
+            //gfx.MFEH = PdfFontEmbedding.Default;
+
+            XFont font = new XFont("Verdana", 13, XFontStyle.Bold);
+
+            gfx.DrawString("The following paragraph was rendered using MigraDoc:", font, XBrushes.Black,
+              new XRect(100, 100, page.Width - 200, 300), XStringFormats.Center);
+
+            // You always need a MigraDoc document for rendering.
+            Document doc = new Document();
+            Section sec = doc.AddSection();
+            // Add a single paragraph with some text and format information.
+            Paragraph para = sec.AddParagraph();
+            para.Format.Alignment = ParagraphAlignment.Justify;
+            para.Format.Font.Name = "Times New Roman";
+            para.Format.Font.Size = 12;
+            para.Format.Font.Color = MigraDoc.DocumentObjectModel.Colors.DarkGray;
+            para.Format.Font.Color = MigraDoc.DocumentObjectModel.Colors.DarkGray;
+            para.AddText("Duisism odigna acipsum delesenisl ");
+            para.AddFormattedText("ullum in velenit", TextFormat.Bold);
+            para.AddText(" ipit iurero dolum zzriliquisis nit wis dolore vel et nonsequipit, velendigna " +
+              "auguercilit lor se dipisl duismod tatem zzrit at laore magna feummod oloborting ea con vel " +
+              "essit augiati onsequat luptat nos diatum vel ullum illummy nonsent nit ipis et nonsequis " +
+              "niation utpat. Odolobor augait et non etueril landre min ut ulla feugiam commodo lortie ex " +
+              "essent augait el ing eumsan hendre feugait prat augiatem amconul laoreet. ≤≥≈≠");
+            para.Format.Borders.Distance = "5pt";
+            para.Format.Borders.Color = Colors.Gold;
+
+            // Create a renderer and prepare (=layout) the document
+            MigraDoc.Rendering.DocumentRenderer docRenderer = new DocumentRenderer(doc);
+            docRenderer.PrepareDocument();
+
+            //// Render the paragraph. You can render tables or shapes the same way.
+            docRenderer.RenderObject(gfx, XUnit.FromCentimeter(5), XUnit.FromCentimeter(10), "12cm", para);
+        }
+
+
     }
 }
